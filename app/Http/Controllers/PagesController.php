8ef38bdc6922ea757e075910c0bb1ac23caffc97
zeng <shuangjiezeng@gmail.com>
2019-12-29 14:16:35 +0000

    基础布局

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    //root
+    public function root()
+    {
+        return view('pages.root');
+    }
+}
